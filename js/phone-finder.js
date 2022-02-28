c80ad80ede03df66bc328a18dc4822da002082bb
iamraihan <trueraihan@gmail.com>
2022-02-28 21:14:58 +0600

    fetch api added

--- js/phone-finder.js
+++ js/phone-finder.js
@@ -1,5 +1,11 @@
+// search button event listener 
 const searchButton = () => {
     const searchInput = document.getElementById('search-input')
     let input = searchInput.value
-    console.log(input);
+
+    // fetch api 
+    const url = `https://openapi.programming-hero.com/api/phones?search=${input}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.data))
 }
