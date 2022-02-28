34a56d6c5b051015a15e57ba8303138dad20f067
iamraihan <trueraihan@gmail.com>
2022-03-01 01:47:29 +0600

    website almost ready

--- js/phone-finder.js
+++ js/phone-finder.js
@@ -1,19 +1,38 @@
+const phoneContainer = document.getElementById('phone-container')
+const error = document.getElementById('error')
+
 // search button event listener 
 const searchButton = () => {
     const searchInput = document.getElementById('search-input')
     let input = searchInput.value
 
+
+    if (input === '') {
+        error.style.display = 'block'
+        error.innerHTML = 'Must input a phone name!!'
+        return
+    }
     // fetch api 
     const url = `https://openapi.programming-hero.com/api/phones?search=${input}`
     fetch(url)
         .then(res => res.json())
-        .then(data => getPhone(data.data))
+        .then(data => {
+            if (data.status === 'false') {
+                // console.log(data.data)
+                error.innerHTML = 'Please input a valid phone name'
+            } else {
+                getPhone(data.data)
+            }
+        })
+
+    searchInput.value = ''
 }
 const getPhone = (phones) => {
-    // const max20 = phones.slice(0,20)
-    const phoneContainer = document.getElementById('phone-container')
-    phones.forEach(phone => {
-        console.log(phone)
+    const max20 = phones.slice(0, 20)
+    // const phoneContainer = document.getElementById('phone-container')
+    max20.forEach(phone => {
+        error.style.display = 'none'
+        // console.log(phone)
         const div = document.createElement('div')
         div.classList.add('col')
         div.innerHTML = `
@@ -45,6 +64,15 @@ const showPhoneDetais = (info) => {
     <div class="card-body">
       <h5 class="card-title">Model: ${info.name}</h5>
       <p class="card-text">Brand: ${info.brand}</p>
+      <p class="card-text"> ${info.releaseDate || 'No Release Date Found'} </p>
+      <p class="card-text">Chipset: ${info.mainFeatures.chipSet}</p>
+      <p class="card-text">Display: ${info.mainFeatures.displaySize}</p>
+      <p class="card-text">Storage: ${info.mainFeatures.storage}</p>
+      <p class="card-text">Memory: ${info.mainFeatures.memory}</p>
+      <p class="card-text">Sensors: ${info.mainFeatures.sensors[0]}, ${info.mainFeatures.sensors[1]}, ${info.mainFeatures.sensors[2]}, ${info.mainFeatures.sensors[3]}, ${info.mainFeatures.sensors[4]}, ${info.mainFeatures.sensors[5]}</p>
+
+        
+
     </div>
   </div>
     `
