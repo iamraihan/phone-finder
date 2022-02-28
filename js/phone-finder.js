3299fc15b42115dce9e7573b2e06e1d7be8ed29d
iamraihan <trueraihan@gmail.com>
2022-02-28 21:37:12 +0600

    find phone aded

--- js/phone-finder.js
+++ js/phone-finder.js
@@ -7,5 +7,28 @@ const searchButton = () => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${input}`
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data))
+        .then(data => getPhone(data.data))
+}
+const getPhone = (phones) => {
+    // const max20 = phones.slice(0,20)
+    const phoneContainer = document.getElementById('phone-container')
+    phones.forEach(phone => {
+        console.log(phone)
+        const div = document.createElement('div')
+        div.classList.add('col')
+        div.innerHTML = `
+        <div class="card">
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${phone.phone_name}</h5>
+            <p class="card-text">${phone.brand}</p>
+            </div>
+            <button class="btn btn-primary">Show Details</button>
+
+        </div>
+        `
+        phoneContainer.appendChild(div)
+    })
+
+
 }
